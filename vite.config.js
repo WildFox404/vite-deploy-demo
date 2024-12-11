0e124892ceb4c84e64d0540064240dc66a61a262
WildFox404 <3220429027@qq.com>
2024-12-12 00:56:02 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src',
-      'assets': 'src/assets',
-      // 可以根据需要添加更多的别名
-    },
-  },
-  base:"/vite-deploy-demo/",
-  plugins: [react()],
+  plugins: [vue()],
 })
