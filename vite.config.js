48c5558772fa6160a31edfaa4cefe4e5904861e7
WildFox404 <3220429027@qq.com>
2025-01-24 22:10:47 +0800

    9999

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     alias:{
       '@':'/src',
       '@assets':'/src/assets',
-      '@component':'/src/component',
+      '@mycomponent':'/src/component',
     }
   },
   base : '/vite-deploy-demo/'
